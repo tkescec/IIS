b68fa21f755e0714939a41538d0de660c75fc169
Tomislav Kescec <tomislav.kescec@gmail.com>
2023-04-01 09:59:15 +0200

    Initial commit

---
+++ src/main/java/com/codetome/iis/controllers/HomeController.java
@@ -0,0 +1,2 @@
+package com.codetome.iis.controllers;public class HomeController {
+}
---
+++ src/test/java/com/codetome/iis/IisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codetome.iis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
