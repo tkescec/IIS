33093be3535844679fdaf25b6d9c2bffcdcbd2b6
Tomislav Kescec <tomislav.kescec@gmail.com>
2023-04-06 17:13:32 +0200

    Add home controller

--- src/main/java/com/codetome/iis/controllers/HomeController.java
+++ src/main/java/com/codetome/iis/controllers/HomeController.java
@@ -1,2 +1,28 @@
-package com.codetome.iis.controllers;public class HomeController {
+package com.codetome.iis.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/xsd")
+    public String xsd() {
+        return "xsd";
+    }
+
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
 }
